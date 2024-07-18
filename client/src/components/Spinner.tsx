a1185e6f67d3cf2f8edbefb59d5d71d1216847bb
irishabhnegi <rishabhnegi175@gmail.com>
2024-07-18 16:16:36 +0530

    spinner err

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 const Spinner = () => {
   return (
-    <Loader className='flex-1 h-auto m-0 mx-auto text-center animate-spin size-10' />
+    <Loader className='flex-1 m-0 mx-auto text-center h-min animate-spin size-10' />
   );
 };
 export default Spinner;
